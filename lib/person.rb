54433b766547fdc2c26cc424417675361d94d06b
NickeaBennett <nickeabennett@gmail.com>
2022-07-23 13:05:42 -0400

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -57,5 +57,22 @@ class Person
     def call_friend(friend)
         self.happiness += 3
         friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        case topic
+            when 'politics'
+                self.happiness -= 2
+                friend.happiness -= 2
+                "blah blah partisan blah lobbyist"
+            when 'weather'
+                self.happiness += 1
+                friend.happiness += 1
+                "blah blah sun blah rain"
+            else
+                
+                "blah blah blah blah blah"
+            end
     end
 end
