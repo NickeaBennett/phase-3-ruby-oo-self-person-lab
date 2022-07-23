c3e6196f89509f33779d54bba47f8482d9b8c726
NickeaBennett <nickeabennett@gmail.com>
2022-07-23 11:54:13 -0400

    Take a bath

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,54 @@
 # your code goes here
+class Person
+    attr_accessor :bank_account 
+    attr_reader :name, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def hygiene=(value)
+        @hygiene = if value > 10
+                        10
+                    elsif value < 0
+                        0
+                    else
+                        value
+                    end
+    end
+    
+    def happiness=(value)
+        @happiness = if value > 10
+                        10
+                    elsif value < 0
+                        0
+                    else
+                        value
+                    end
+    end
+
+    def happy?
+        self.happiness > 7
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        
+    end
+end
